c25d9fb16ac169fba417215e2d698e8585083f62
igrekus <upload234@mail.ru>
2020-07-28 14:07:28 +0300

    WIP: implement sorted raw measurement data dump

--- statwidget.py
+++ statwidget.py
@@ -1,8 +1,13 @@
+import datetime
+import openpyxl
 import os
 
 from PyQt5 import uic
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QWidget
+
+
+from measureresult import phs_value_for_phs_code, att_value_for_att_code, chunks
 
 
 class StatWidget(QWidget):
@@ -32,6 +37,57 @@ class StatWidget(QWidget):
     def stats(self, text):
         self._ui.texteditStat.setPlainText(text)
 
-
     def _exportExcel(self):
-        print('exporting')
+
+        def build_title(titles):
+            cells = []
+            for title in titles:
+                for att, phs in title:
+                    cells.append(f'{att} dB, {phs}°')
+
+                cells.append('')
+                cells.append('F, GHz')
+            return ['F, GHz'] + cells[:-1]
+
+        def build_data(freqs, ampss):
+            rows = [list() for _ in range(len(freqs))]
+            for amps in ampss:
+                for row, freq, *amp in zip(rows, freqs, *amps):
+                    row += [freq] + amp + ['']
+            return rows
+
+        unique_phases = set(self._result._phase_codes)
+        chunk_len = len(unique_phases)
+
+        title_data = [(att_value_for_att_code(att), phs_value_for_phs_code(phs)) for att, phs in zip(self._result._att_codes, self._result._phase_codes)]
+        title_data = [chunk for chunk in chunks(title_data, chunk_len)]
+
+        freqs = self._result.freqs
+        s21_amps = [chunk for chunk in chunks(self._result.s21, chunk_len)]
+        s21_phs = [chunk for chunk in chunks(self._result.phase, chunk_len)]
+        s11_amps = [chunk for chunk in chunks(self._result.s11, chunk_len)]
+        s12_amps = [chunk for chunk in chunks(self._result.s12, chunk_len)]
+        s22_amps = [chunk for chunk in chunks(self._result.s22, chunk_len)]
+        titles = build_title(title_data)
+
+        wb = openpyxl.Workbook()
+        ws = wb.active
+        ws.title = 'S21 amps'
+
+        ws.append(titles)
+        for row in build_data(freqs, s21_amps):
+            ws.append(row)
+
+        ws = wb.create_sheet('S21 phases')
+        ws.append(['stub'])
+
+        if not os.path.isdir('xlsx'):
+            os.mkdir('xlsx')
+
+        wb.save(f'xlsx\\{self.xlsx_filename}')
+        os.startfile('xlsx')
+
+
+    @property
+    def xlsx_filename(self):
+        return f'att-psm_{datetime.datetime.now().isoformat().replace(":","-")}.xlsx'
